c4610248753d5e4d3ab9ccc2c03cd11ae377fe97
Zoe van Ammers <johannacows52@gmail.com>
2024-07-06 19:18:22 +1000

    feat: add query string check

--- httpserver/main.go
+++ httpserver/main.go
@@ -14,7 +14,19 @@ const keyServerAddr = "serverAddr"
 // getRoot returns the root request
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	fmt.Printf("%s: got / request\n", ctx.Value(keyServerAddr))
+
+	hasFirst := r.URL.Query().Has("first")
+	first := r.URL.Query().Get("first")
+	hasSecond := r.URL.Query().Has("second")
+	second := r.URL.Query().Get("second")
+
+	fmt.Printf("%s: got / request. first(%t)=%s, second(%t)=%s\n",
+		ctx.Value(keyServerAddr),
+		hasFirst,
+		first,
+		hasSecond,
+		second,
+	)
 	io.WriteString(w, "You've hit the root of my website!\n")
 }
 
@@ -31,7 +43,7 @@ func main() {
 	mux.HandleFunc("/", getRoot)
 	mux.HandleFunc("/hello", getHello)
 
-	ctx, cancelCtx := context.WithCancel(context.Background())
+	ctx := context.Background()
 	serverOne := &http.Server{
 		Addr:    ":3333",
 		Handler: mux,
@@ -41,41 +53,11 @@ func main() {
 		},
 	}
 
-	serverTwo := &http.Server{
-		Addr:    ":4444",
-		Handler: mux,
-		BaseContext: func(l net.Listener) context.Context {
-			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
-			return ctx
-		},
+	err := serverOne.ListenAndServe()
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
 	}
-
-	go func() {
-		err := serverOne.ListenAndServe()
-		if errors.Is(err, http.ErrServerClosed) {
-			fmt.Printf("server one closed\n")
-		} else if err != nil {
-			fmt.Printf("error listening for server one: %s\n", err)
-		}
-		cancelCtx()
-	}()
-
-	go func() {
-		err := serverTwo.ListenAndServe()
-		if errors.Is(err, http.ErrServerClosed) {
-			fmt.Printf("server one closed\n")
-		} else if err != nil {
-			fmt.Printf("error listening for server two: %s\n", err)
-		}
-		cancelCtx()
-	}()
-	<-ctx.Done()
-
-	// err := http.ListenAndServe(":3333", mux)
-
-	// if errors.Is(err, http.ErrServerClosed) {
-	// 	fmt.Printf("server closed\n")
-	// } else if err != nil {
-	// 	fmt.Printf("error starting server: %s\n", err)
-	// }
 }
